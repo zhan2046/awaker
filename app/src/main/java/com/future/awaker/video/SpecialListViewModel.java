532d4532be926b8de7748c06133776163d21cadf
ruzhan123 <dev19921116@gmail.com>
2017-07-28 11:37:28 +0800

    add special list page cache

--- app/src/main/java/com/future/awaker/video/SpecialListViewModel.java
+++ app/src/main/java/com/future/awaker/video/SpecialListViewModel.java
@@ -4,17 +4,24 @@ import android.databinding.Bindable;
 
 import com.future.awaker.base.BaseListViewModel;
 import com.future.awaker.data.SpecialDetail;
+import com.future.awaker.data.realm.SpecialDetailRealm;
 import com.future.awaker.data.source.NewRepository;
 import com.future.awaker.network.EmptyConsumer;
 import com.future.awaker.network.ErrorConsumer;
+import com.future.awaker.util.LogUtils;
+
+import java.util.HashMap;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.realm.RealmResults;
 
 /**
  * Created by ruzhan on 2017/7/15.
  */
 
 public class SpecialListViewModel extends BaseListViewModel {
+
+    private static final String TAG = SpecialListViewModel.class.getSimpleName();
 
     private String id;
     private String title;
@@ -23,6 +30,8 @@ public class SpecialListViewModel extends BaseListViewModel {
 
     @Bindable
     private SpecialDetail specialDetail;
+
+    private HashMap<String, String> map = new HashMap<>();
 
     public SpecialDetail getSpecialDetail() {
         return specialDetail;
@@ -58,20 +67,59 @@ public class SpecialListViewModel extends BaseListViewModel {
         this.id = id;
         this.title = title;
         this.url = url;
+        map.put(SpecialDetailRealm.ID, id);
     }
 
     @Override
     public void refreshData(boolean refresh) {
+        if (isRefresh && specialDetail == null) {
+            getLocalSpecialDetail();
+        }
+        getRemoteSpecialDetail();
+    }
+
+    private void getRemoteSpecialDetail() {
         disposable.add(NewRepository.get()
                 .getSpecialDetail(TOKEN, id)
                 .observeOn(AndroidSchedulers.mainThread())
                 .doOnError(throwable -> isError.set(throwable))
                 .doOnSubscribe(disposable -> isRunning.set(true))
                 .doOnTerminate(() -> isRunning.set(false))
-                .doOnNext(result -> {
-                    SpecialDetail specialDetail = result.getData();
-                    setSpecialDetail(specialDetail);
+                .doOnNext(result -> setRemoteSpecialDetail(result.getData()))
+                .subscribe(new EmptyConsumer(), new ErrorConsumer()));
+    }
+
+    private void setRemoteSpecialDetail(SpecialDetail specialDetail) {
+        checkEmpty(specialDetail);
+        if (!isEmpty.get()) {
+            if (isRefresh) {
+                // save to local
+                SpecialDetailRealm newDetailRealm = SpecialDetailRealm.setSpecialDetail(specialDetail);
+                NewRepository.get().updateLocalRealm(newDetailRealm);
+            }
+            setSpecialDetail(specialDetail);
+        }
+    }
+
+    private void getLocalSpecialDetail() {
+        disposable.add(NewRepository.get().getLocalRealm(SpecialDetailRealm.class, map)
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnError(throwable -> LogUtils.showLog(TAG, "doOnError: " + throwable.toString()))
+                .doOnNext(realmResults -> {
+                    LogUtils.d("getLocalSpecialDetail" + realmResults.size());
+                    setLocalSpecialDetail(realmResults);
                 })
                 .subscribe(new EmptyConsumer(), new ErrorConsumer()));
+    }
+
+    private void setLocalSpecialDetail(RealmResults realmResults) {
+        if (realmResults == null || realmResults.isEmpty()) {
+            return;
+        }
+        SpecialDetailRealm commentHotRealm = (SpecialDetailRealm) realmResults.get(0);
+        if (specialDetail == null) {   // data is empty, network not back
+            SpecialDetail specialDetail = SpecialDetailRealm.setSpecialDetailRealm(commentHotRealm);
+            setSpecialDetail(specialDetail);
+        }
     }
 }
