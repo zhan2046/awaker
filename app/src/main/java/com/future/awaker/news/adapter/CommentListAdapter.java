548911287aa859a1d885e1025d2c7e9b572b2fc6
ruzhan123 <dev19921116@gmail.com>
2017-08-17 21:17:24 +0800

    update comment list

--- app/src/main/java/com/future/awaker/news/adapter/CommentListAdapter.java
+++ app/src/main/java/com/future/awaker/news/adapter/CommentListAdapter.java
@@ -15,7 +15,6 @@ import com.future.awaker.databinding.ItemLoadBinding;
 import com.future.awaker.databinding.ItemNewDetailCommentBinding;
 import com.future.awaker.news.adapter.holder.NewDetailCommentHolder;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -40,9 +39,6 @@ public class CommentListAdapter extends RecyclerView.Adapter {
         if (comments == null || comments.isEmpty()) {
             return;
         }
-
-        Collections.reverse(comments);
-
         if (commentList == null) {
             commentList = comments;
             notifyDataSetChanged();
