3dc3988b50971ae5d90a842aeb6513fac9e6e206
ruzhan123 <dev19921116@gmail.com>
2018-01-09 16:14:57 +0800

    update newsEntity model

--- app/src/main/java/com/future/awaker/db/entity/NewsEntity.java
+++ app/src/main/java/com/future/awaker/db/entity/NewsEntity.java
@@ -42,9 +42,15 @@ public class NewsEntity {
     public String category_title;
     public String category_color;
     @Embedded(prefix = "category_name")
-    private CategoryNameEntity category_name;
+    public CategoryNameEntity category_name;
 
-    public NewsEntity(String id, String uid, String title, String description, String category, String status, String reason, String sort, String position, String cover, String view, String comment, String collection, String dead_line, String source, String create_time, String update_time, CoverUrlEntity cover_url, String approval, String share_url, UserEntity user, String category_title, String category_color, CategoryNameEntity category_name) {
+    public NewsEntity(String id, String uid, String title, String description, String category,
+                      String status, String reason, String sort, String position, String cover,
+                      String view, String comment, String collection, String dead_line,
+                      String source, String create_time, String update_time,
+                      CoverUrlEntity cover_url, String approval, String share_url, UserEntity user,
+                      String category_title, String category_color,
+                      CategoryNameEntity category_name) {
         this.id = id;
         this.uid = uid;
         this.title = title;
@@ -155,197 +161,5 @@ public class NewsEntity {
             newsList.add(newsEntity);
         }
         return newsList;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-    public String getSort() {
-        return sort;
-    }
-
-    public void setSort(String sort) {
-        this.sort = sort;
-    }
-
-    public String getPosition() {
-        return position;
-    }
-
-    public void setPosition(String position) {
-        this.position = position;
-    }
-
-    public String getCover() {
-        return cover;
-    }
-
-    public void setCover(String cover) {
-        this.cover = cover;
-    }
-
-    public String getView() {
-        return view;
-    }
-
-    public void setView(String view) {
-        this.view = view;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
-
-    public String getCollection() {
-        return collection;
-    }
-
-    public void setCollection(String collection) {
-        this.collection = collection;
-    }
-
-    public String getDead_line() {
-        return dead_line;
-    }
-
-    public void setDead_line(String dead_line) {
-        this.dead_line = dead_line;
-    }
-
-    public String getSource() {
-        return source;
-    }
-
-    public void setSource(String source) {
-        this.source = source;
-    }
-
-    public String getCreate_time() {
-        return create_time;
-    }
-
-    public void setCreate_time(String create_time) {
-        this.create_time = create_time;
-    }
-
-    public String getUpdate_time() {
-        return update_time;
-    }
-
-    public void setUpdate_time(String update_time) {
-        this.update_time = update_time;
-    }
-
-    public CoverUrlEntity getCover_url() {
-        return cover_url;
-    }
-
-    public void setCover_url(CoverUrlEntity cover_url) {
-        this.cover_url = cover_url;
-    }
-
-    public String getApproval() {
-        return approval;
-    }
-
-    public void setApproval(String approval) {
-        this.approval = approval;
-    }
-
-    public String getShare_url() {
-        return share_url;
-    }
-
-    public void setShare_url(String share_url) {
-        this.share_url = share_url;
-    }
-
-    public UserEntity getUser() {
-        return user;
-    }
-
-    public void setUser(UserEntity user) {
-        this.user = user;
-    }
-
-    public String getCategory_title() {
-        return category_title;
-    }
-
-    public void setCategory_title(String category_title) {
-        this.category_title = category_title;
-    }
-
-    public String getCategory_color() {
-        return category_color;
-    }
-
-    public void setCategory_color(String category_color) {
-        this.category_color = category_color;
-    }
-
-    public CategoryNameEntity getCategory_name() {
-        return category_name;
-    }
-
-    public void setCategory_name(CategoryNameEntity category_name) {
-        this.category_name = category_name;
     }
 }
--- app/src/main/java/com/future/awaker/news/fragment/NewListFragment.java
+++ app/src/main/java/com/future/awaker/news/fragment/NewListFragment.java
@@ -1,7 +1,6 @@
 package com.future.awaker.news.fragment;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.View;
 
