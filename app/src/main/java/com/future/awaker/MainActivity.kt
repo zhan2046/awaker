977a55026c7e8ae6f9ae2e9b2c258443cb032b10
ruzhan123 <dev19921116@gmail.com>
2019-05-21 10:24:03 +0800

    fix main page init fragment logic

--- app/src/main/java/com/future/awaker/MainActivity.kt
+++ app/src/main/java/com/future/awaker/MainActivity.kt
@@ -3,20 +3,13 @@ package com.future.awaker
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-/**
- * Created by ruzhan on 2017/7/12.
- */
-
 class MainActivity : AppCompatActivity() {
-
-    private var mainFragment: MainFragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.container)
-
-        if (mainFragment == null) {
-            mainFragment = MainFragment.newInstance()
+        if (savedInstanceState == null) {
+            val mainFragment = MainFragment.newInstance()
             supportFragmentManager
                     .beginTransaction()
                     .add(R.id.container, mainFragment, "MainFragment")
