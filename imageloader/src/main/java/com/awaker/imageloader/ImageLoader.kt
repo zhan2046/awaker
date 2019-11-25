732c77c7a5a16325f78cc930f5629f7ded8f3753
ruzhan123 <dev19921116@gmail.com>
2019-11-25 16:32:02 +0800

    update image loader get method

--- imageloader/src/main/java/com/awaker/imageloader/ImageLoader.kt
+++ imageloader/src/main/java/com/awaker/imageloader/ImageLoader.kt
@@ -5,14 +5,7 @@ object ImageLoader {
 
     private var imageLoader: IImageLoader? = null
 
-    fun get(): IImageLoader {
-        if (imageLoader == null) {
-            synchronized(ImageLoader::class.java) {
-                if (imageLoader == null) {
-                    imageLoader = GlideImpl()
-                }
-            }
-        }
-        return imageLoader!!
+    fun get(): IImageLoader = imageLoader ?: synchronized(ImageLoader::class.java) {
+        imageLoader ?: GlideImpl().also { imageLoader = it }
     }
 }
