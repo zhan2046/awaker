8eda012ada680fb529e7e3b8a2a4324ec60985e3
ruzhan123 <dev19921116@gmail.com>
2020-02-17 17:12:31 +0800

    opt image detail system navigation hide

--- day/src/main/java/com/ruzhan/day/image/DayImageDetailActivity.kt
+++ day/src/main/java/com/ruzhan/day/image/DayImageDetailActivity.kt
@@ -1,9 +1,12 @@
 package com.ruzhan.day.image
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.graphics.PixelFormat
+import android.os.Build
 import android.os.Bundle
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
@@ -29,6 +32,7 @@ class DayImageDetailActivity : AppCompatActivity() {
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN)
         window.setFormat(PixelFormat.TRANSLUCENT)
+        hideSystemNavigationBar()
         setContentView(R.layout.day_container)
         val imageUrl = intent?.getStringExtra(IMAGE_URL) ?: ""
         val frag = DayImageDetailFragment.newInstance(imageUrl)
@@ -36,5 +40,18 @@ class DayImageDetailActivity : AppCompatActivity() {
             .beginTransaction()
             .add(R.id.container, frag, "DayImageDetailFragment")
             .commit()
+    }
+
+    @SuppressLint("ObsoleteSdkInt")
+    private fun hideSystemNavigationBar() {
+        if (Build.VERSION.SDK_INT in 12..18) {
+            val view = this.window.decorView
+            view.systemUiVisibility = View.GONE
+        } else if (Build.VERSION.SDK_INT >= 19) {
+            val decorView = window.decorView
+            val uiOptions = (View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN)
+            decorView.systemUiVisibility = uiOptions
+        }
     }
 }
